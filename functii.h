f7aafbdf2ad81f13149ea71e68c020191099b9e5
Corches Ciprian <cipriancorches@gmail.com>
2023-02-05 20:03:15 +0200

    problema 2 finalizata

--- functii.h
+++ functii.h
@@ -85,19 +85,23 @@ void secventiereSol2(int v[], int dim) {
 	int smax = 1;
 	int dmax = 0;
 	for (int i = 0; i < dim; i++) {
-		if (numarPrim(v[i]) == true && numarPrim(v[i+1]) == true  && v[i]< v[i+1] &&  numarPrim(oglindit(v[i])) == true && numarPrim(oglindit(v[i])) == true) {
+		if (numarPrim(v[i]) == true && numarPrim(v[i+1]) == true  &&   numarPrim(oglindit(v[i])) == true && numarPrim(oglindit(v[i+1])) == true) {
 			int j = i;
-			while (j + 1 < dim && numarPrim(v[j]) == true && numarPrim(v[j+ 1]) == true && v[j] < v[j+ 1] && numarPrim(oglindit(v[j])) == true && numarPrim(oglindit(v[j])) == true) {
+			while (j + 1 < dim && numarPrim(v[j]) == true && numarPrim(v[j+ 1]) == true &&  numarPrim(oglindit(v[j])) == true && numarPrim(oglindit(v[j+1])) == true) {
 				j++;
 			}
-			if (j - i + 1 > dmax - smax + 1) {
+			if (j - i + 1 > dmax - smax + 1) {;
 				smax = i, dmax = j;
 
 			}
-
 		}
 	}
-	cout << smax << " " << dmax << endl;
+	for (int f = 0; f < dim; f++) {
+		if (f >= smax && f <= dmax) {
+			cout << v[f] << " ";
+		}
+	}
+	cout << endl;
 }
 
 
